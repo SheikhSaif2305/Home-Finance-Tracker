b4d84e549541e6d6b67e5e2decda7f1891e8b538
Saif Sheikh <sheikhsaif2305@gmail.com>
2024-11-19 02:17:38 +0600

    Balance Updated again!!

--- models/Balance.js
+++ models/Balance.js
@@ -1,12 +1,17 @@
 const pool = require('../config/db');
 
-// Balance object carries two functons with key findByUderId and updateBalance
-
+// Balance object carries 
 const Balance = { 
-  findByUserId: function(userId) {
-    return pool.query('SELECT * FROM Balance WHERE user_id = ?', [userId])
-      .then(([rows]) => rows[0]);  // Returns the first row (or undefined if no row found)
+  findByUserId: async function(userId) {
+    const [row] = await pool.query('SELECT * FROM Balance WHERE user_id = ?', [userId]);
+    //console.log(row);
+
+    return row[0]; 
+    // Returns the first row (or undefined if no row found)
+
   },
+
+  // Update Balance not used yet
 
   updateBalance: function(balanceId, newBalance) {
     return pool.query('UPDATE Balance SET current_balance = ? WHERE balance_id = ?', [newBalance, balanceId]);
